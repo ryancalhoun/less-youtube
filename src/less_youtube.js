e406726a97d3a0bb661faf97fbbfda6c063c4c93
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2018-01-14 18:51:33 -0600

    Tracking minutes, not just a start and end time

--- src/less_youtube.js
+++ src/less_youtube.js
@@ -1,39 +1,58 @@
 (function() {
 
-  var limit = 20 * 60 * 1000;
-  var cooldown = 60 * 60 * 1000;
+  var limit = 20;
+  var reset = 60 * 60 * 1000;
+  var cooldown = 5 * 60 * 1000;
+  var blocked = false;
 
-  function startCountdown(startTime) {
-    localStorage.setItem('_youtube_start_time', startTime);
-    var endTime = new Date(startTime.getTime() + limit);
-    var clearTime = new Date(startTime.getTime() + cooldown);
+  updateMinutes();
 
-    console.log("Countdown to ", endTime);
+  function updateMinutes() {
+    setTimeout(updateMinutes, 60 * 1000);
 
-    setTimeout(function() {
-      document.body.innerHTML = '<html><head><style>body {max-width: 740px; margin: 80px auto; font-size: 18px};</style></head><body><h2>no more youtube until</h2><h1>' + clearTime + '</h1></body></html>';
-    }, endTime - new Date());
+    var now = new Date();
+    var minutes = localStorage.getItem('_youtube_minutes');
 
-    setTimeout(function() {
-      window.location.reload();
-    }, clearTime - new Date() + 100);
-  }
+    if(minutes) {
+      minutes = JSON.parse(minutes);
+    } else {
+      minutes = [];
+    }
 
-  var now = new Date();
-  var startTime = localStorage.getItem('_youtube_start_time');
+    var initialTotal = minutes.length;
+    while(minutes.length > 0) {
+      var time = new Date(minutes[0]);
+      if(time.getTime() + reset < now.getTime()) {
+        minutes.shift();
+      } else {
+        break;
+      }
+    }
 
-  if(startTime) {
-    startTime = new Date(startTime);
-  } else {
-    startTime = now;
-  }
+    if(minutes.length >= limit) {
+      var lastMinute = new Date(minutes[minutes.length - limit]);
+      var nextMinute = new Date(lastMinute.getTime() + reset + cooldown);
 
-  var clearTime = new Date(startTime.getTime() + cooldown);
+      console.log("Time's up. Next time", nextMinute);
 
-  if(now < clearTime) {
-    startCountdown(startTime);
-  } else {
-    startCountdown(now);
+      document.body.innerHTML = '<html><head><style>body {max-width: 740px; margin: 80px auto; font-size: 18px};</style></head><body><h2>no more youtube until</h2><h1>' + nextMinute + '</h1></body></html>';
+
+      blocked = true;
+    } else {
+
+      if(blocked) {
+        setTimeout(window.location.reload, cooldown);
+
+      } else {
+        if(minutes.length == 0 || (now.getTime() - new Date(minutes[minutes.length-1]).getTime() >= 50 * 1000)) {
+          minutes.push(now);
+        }
+        console.log("Currently viewing " + minutes.length + " out of " + limit);
+
+        localStorage.setItem('_youtube_minutes', JSON.stringify(minutes));
+      }
+
+    }
   }
 
 })();
